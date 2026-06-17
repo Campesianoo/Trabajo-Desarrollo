77714e6578840d1b739e6c6c12b7501db5ee9d8f
Valentino Docheff <valentinodocheff@gmail.com>
2026-06-17 12:36:14 -0300

    creada estructura base de Especialidad

---
+++ backend/src/models/Especialidad.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Especialidad = sequelize.define('Especialidad', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Especialidad;
